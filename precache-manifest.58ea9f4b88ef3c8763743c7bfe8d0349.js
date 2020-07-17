17c1d60f53e3abab3b398216b75253d3d4b4ede4
Dylan <dylc2770@gmail.com>
2020-07-17 19:19:31 +0200

    Updates

---
+++ precache-manifest.58ea9f4b88ef3c8763743c7bfe8d0349.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e582b20a3e080926704d561172575ea4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11c1a1dc09514704f2bb",
+    "url": "/static/js/2.b8ba72be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b8ba72be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a57bc572c22900bd9699",
+    "url": "/static/js/main.ae048d9c.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
--- src/components/button.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-const Button = styled.button`
-  padding: 10px;
-`;
-
-export default Button;
